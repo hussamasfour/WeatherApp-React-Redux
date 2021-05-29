b436340d51d1e5151bb7de5a9491d3b8d4015217
hussam asfour <hussam_asfour@yahoo.com>
2021-05-29 11:36:27 -0400

    intial commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
